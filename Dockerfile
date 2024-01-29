a5b5d97a224175188f19da38744aaea5453434eb
MatheusBenetti <mbbenetti1@gmail.com>
2024-01-29 19:43:41 -0300

    adjust dockerfile and tests

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,10 @@
-FROM golang:1.17-alpine as builder
-
+FROM golang:1.21 as builder
 WORKDIR /app
-
 COPY go.mod go.sum ./
-
 COPY . .
-
 RUN CGO_ENABLED=0 GOOS=linux go build -o temp-system .
 
-FROM alpine:latest
-
+FROM scratch
 WORKDIR /app
-
 COPY --from=builder /app/temp-system .
-
-CMD ["./temp-system", "run"]
+CMD ["./temp-system"]
--- test_main.go
+++ test_main.go
@@ -1,42 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-	"net/http/httptest"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
+	"time"
 )
 
-func testTempSystem(t *testing.T) {
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Simulate the Weather API response
-		responseJSON := `{"location":{"name":"TestLocation"},"current":{"temp_c":20.0,"temp_f":68.0,"temp_k":293.15}}`
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(responseJSON))
-	}))
-	defer testServer.Close()
-
-	// Save the original Weather API URL and set the test server URL
-	originalURL := weatherAPIURL
-	weatherAPIURL = testServer.URL
-	defer func() {
-		// Reset the original Weather API URL after the test
-		weatherAPIURL = originalURL
-	}()
-
-	// Call the function with a test location
-	location := "TestLocation"
-	result, err := fetchWeatherAPI(location)
-	assert.NoError(t, err, "fetchWeatherAPI should not return an error")
-
-	// Verify the expected values using testify's assert package
-	expectedResult := &WeatherResponse{
-		TempC: 20.0,
-		TempF: 68.0,
-		TempK: 293.15,
+func TestValidCep(t *testing.T) {
+	go main()
+	time.Sleep(1 * time.Second)
+	resp, err := http.Get("http://localhost:8080/getTemperature?cep=12345678")
+	if err != nil {
+		t.Fatalf("failed to make GET request: %v", err)
 	}
-
-	assert.Equal(t, expectedResult, result, "Unexpected result")
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+	var data Current
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		t.Fatalf("failed to decode response body: %v", err)
+	}
 }
